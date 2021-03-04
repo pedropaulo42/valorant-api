1b900ff3ba081022d940663d08cfb35ee53d9e42
Pedro Paulo <pedroppaulo42@gmail.com>
2021-03-03 21:23:15 -0300

    Created Project

---
+++ src/test/java/com/vaval/valorantapi/ValorantApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vaval.valorantapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ValorantApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
